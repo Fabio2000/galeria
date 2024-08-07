2a08b2856763c101ff21d77da7fac4559829c079
Fabio2000 <frsilva2101@gmail.com>
2024-08-06 23:32:28 -0300

    estruturando a pagina do amor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Headers from '../src/components/header/header'
-
+import Cards from '../src/components/cards/cards'
 function App() {
   return (
     <section id='containerGeral'>
       <Headers className='containerHeader'/>
+      <section>
+        <Cards />
+      </section>
     </section>
   )
 }
